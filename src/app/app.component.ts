18c47cd109fc2f67a1ac8b50968884e5a7556678
Ricardo W Bento <ricardobento@Ricardos-MacBook-Air.local>
2017-12-12 15:50:56 +0000

    added some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class MyApp {
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private auth: AuthService) {
 
     this.auth.getAuthenticatedUser().subscribe(auth => {
-      !auth ? this.rootPage = 'LoginPage': this.rootPage = 'MessagePage';
+      !auth ? this.rootPage = 'LoginPage': this.rootPage = 'TabsPage';
     })
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
